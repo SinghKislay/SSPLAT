1bbab223f5899bab1ef6151bd45229d85f6b3f80
Kislay <kkunalsingh4u@gmail.com>
2020-10-02 14:15:40 +0530

    added database population script

--- ssplat/news/models.py
+++ ssplat/news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ArticleSource(models.Model):
+    source_id = models.CharField(max_length=50)
+    source_name = models.CharField(max_length=50)
+
+class Articles(models.Model):
+    source = models.ForeignKey(ArticleSource, on_delete=models.CASCADE)
+    author = models.CharField(max_length=50, null=True, blank=True)
+    title = models.CharField(max_length=100, null=True, blank=True)
+    Desc = models.CharField(max_length=1000, null=True, blank=True)
+    url = models.CharField(max_length=1000, null=True, blank=True)
+    url_image = models.CharField(max_length=1000, null=True, blank=True)
+    date = models.DateTimeField(null=True)
+    content = models.CharField(max_length=10000, null=True, blank=True)
+
+print("Json parsed and Database populated!")
---
+++ ssplat/populate.py
@@ -0,0 +1,36 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ssplat.settings')
+
+import django
+import datetime
+import dateutil.parser
+django.setup()
+import json
+from news.models import ArticleSource, Articles
+
+f = open('articles.json',)
+json_object = json.loads(f.read())
+
+for obj in json_object:
+    dt = dateutil.parser.parse(obj['publishedAt'])
+    if(obj['source']['id']==None):
+        art_src = ArticleSource.objects.get_or_create(source_id="unknown", source_name=obj["source"]["name"])[0]
+        Articles.objects.get_or_create(source=art_src,
+                                       author=obj["author"],
+                                       title=obj["title"],
+                                       Desc=obj["description"],
+                                       url=obj["url"],
+                                       url_image=obj['urlToImage'],
+                                       date=dt,
+                                       content=obj["content"])
+    else:
+        art_src = ArticleSource.objects.get_or_create(source_id=obj["source"]["id"], source_name=obj["source"]["name"])[0]
+        Articles.objects.get_or_create(source=art_src,
+                                       author=obj["author"],
+                                       title=obj["title"],
+                                       Desc=obj["description"],
+                                       url=obj["url"],
+                                       url_image=obj['urlToImage'],
+                                       date=dt,
+                                       content=obj["content"])
+    
